5ac79300e4e745e551af93450334c0b35967ef82
Sourabh <rathoursourabh5@gmail.com>
2023-12-27 04:12:05 +0530

    added POST request for publishing posts

--- src/components/AddPostCard.jsx
+++ src/components/AddPostCard.jsx
@@ -1,12 +1,42 @@
 import { RxCross2 } from "react-icons/rx";
 import { useDispatch } from "react-redux";
 import { hideAddPostCard } from "../features/addPostCardSlice";
+import { useRef } from "react";
 
 const AddPostCard = () => {
+
   const dispatch = useDispatch();
   const handleHidePostCard = () => {
     dispatch(hideAddPostCard());
   };
+
+  const title = useRef();
+  const description = useRef();
+  const topic = useRef();
+
+  const handlePublishPost = async () => {
+    const token = localStorage.getItem("token");
+    const data = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      headers: {
+        "CONTENT-TYPE": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      body:JSON.stringify({
+        title: title.current.value,
+        description: description.current.value,
+        topic: topic.current.value,
+      })
+    });
+
+    const json = await data.json();
+    console.log(json);
+  
+   dispatch(hideAddPostCard())
+
+
+  };
+
   return (
     <>
       <div className="w-[70%] left-0 right-0 m-auto top-24 h-[85vh] absolute bg-zinc-900 z-10 rounded-md  ">
@@ -19,7 +49,7 @@ const AddPostCard = () => {
         <div className="w-full h-20 px-2  py-2 text-xl font-semibold ">
           <select
             className="rounded-md  w-full h-12 focus:outline-none bg-zinc-800 text-zinc-200"
-            placeholder="Choose "
+            ref={topic}
           >
             <option disabled selected value="">
               Select a Topic
@@ -49,19 +79,26 @@ const AddPostCard = () => {
             className="w-[90%] text-3xl font-semibold bg-zinc-900 rounded px-3 py-2 focus:outline-none  text-zinc-200"
             type="text"
             id="title"
+            ref={title}
             placeholder="Write a specific title..."
           />
         </div>
         <div className="w-full h-[50%] px-2 py-4">
-            <textarea
-                className="w-full h-full text-xl  bg-zinc-900 rounded px-3 py-2 focus:outline-none resize-none text-zinc-200"
-                type="text"
-                id="title"
-                placeholder="Unleash your thoughts and paint the canvas of your imagination..."
-            />
+          <textarea
+            className="w-full h-full text-xl  bg-zinc-900 rounded px-3 py-2 focus:outline-none resize-none text-zinc-200"
+            type="text"
+            ref={description}
+            id="title"
+            placeholder="Unleash your thoughts and paint the canvas of your imagination..."
+          />
         </div>
         <div className="w-full px-4 mb-4 flex justify-end">
-            <button className="bg-zinc-800  text-zinc-100 px-4 py-2 rounded-md hover:bg-red-800 transition duration-300">Publish</button>
+          <button
+            onClick={handlePublishPost}
+            className="bg-zinc-800  text-zinc-100 px-4 py-2 rounded-md hover:bg-red-800 transition duration-300"
+          >
+            Publish
+          </button>
         </div>
       </div>
     </>
