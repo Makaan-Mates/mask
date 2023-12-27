6fca9161da6c0be21c1bbc7c40579a1fb28d14eb
Sourabh <rathoursourabh5@gmail.com>
2023-12-27 12:39:22 +0530

    mapped topics dynamically

---
+++ src/utils/topics.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const topics = [
+{
+    name: "Topic 1",
+    id: uuidv4()
+},
+{
+    name: "Topic 2",
+    id: uuidv4()
+},
+{
+    name: "Topic 3",
+    id: uuidv4()
+},
+{
+    name: "Topic 4",
+    id: uuidv4()
+},
+{
+    name: "Topic 5",
+    id: uuidv4()
+}
+]
+
