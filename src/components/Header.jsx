48ae3e85cf3f19fa7fc3e5ff823db2e31ee04552
shadevkumar <shadevsrk261@gmail.com>
2023-12-27 11:44:25 +0530

    Size issue fixed and Allpost component initiated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,15 +16,15 @@ const Header = () => {
   }
 
   return (
-    <div className="bg-zinc-800 sticky top-0 h-20 text-zinc-100 py-4 px-6  flex items-center justify-between  border-b-[1px] border-zinc-900">
+    <div className="bg-zinc-800 sticky top-0 h-[12vh] text-zinc-200 py-4 px-6  flex items-center justify-between  border-b-[1px] border-zinc-900">
       <div className="flex items-center justify-center space-x-4 gap-5">
         <div className="text-4xl font-bold transition-transform transform hover:scale-105">mask</div>
         <div className="text-l px-2 py-1 font-semibold hover:bg-zinc-700 rounded-md">Home </div>
         <div className="text-l px-2 py-1 font-semibold hover:bg-zinc-700 rounded-md">Explore</div>
-        <div className="bg-zinc-100 px-4 py-2 text-zinc-800  rounded-full w-72 flex items-center">
+        <div className="bg-zinc-200 px-4 py-2 text-zinc-800  rounded-full w-72 flex items-center">
           <input
             type="text"
-            className="w-full items-center  focus:outline-none"
+            className="w-full items-center bg-zinc-200 focus:outline-none"
             placeholder="Search..." />
           <FaSearch className="text-xl text-zinc-800 mx-2" />
         </div>
