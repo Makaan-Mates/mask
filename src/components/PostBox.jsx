bdbc5b064cbe9ef55baa539c96c34d5f4ddc9139
Sourabh <rathoursourabh5@gmail.com>
2023-12-28 14:15:38 +0530

    fetched postDetails from backend and rendered in UI

--- src/components/PostBox.jsx
+++
@@ -1,9 +0,0 @@
-const PostBox = () => {
-  return (
-    <div className="w-full min-h-[88vh] bg-zinc-800 px-5 py-10">
-      <div className="w-full h-full bg-zinc-700">Post Details</div>
-    </div>
-  );
-};
-
-export default PostBox;
